1815b131e849054ba9137e778048a052bbe76b63
HashiramaSenjuhari <truebillionhari@gmail.com>
2025-12-12 15:21:31 +0530

    added date util

---
+++ app/src/main/java/com/example/billionairehari/utils/Date.kt
@@ -0,0 +1,16 @@
+package com.example.billionairehari.utils
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun currentDateTime():String {
+    return LocalDateTime.now(ZoneId.of("Asia/Kolkata"))
+        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+}
+
+fun currentDate():String {
+    return LocalDate.now(ZoneId.of("Asia/Kolkata"))
+        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+}

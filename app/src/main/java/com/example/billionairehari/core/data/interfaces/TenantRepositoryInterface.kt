c68af70d4a8f6e70bf03cf5da9b8d4f2dd45de20
HashiramaSenjuhari <truebillionhari@gmail.com>
2026-01-06 17:48:18 +0530

    Modified the return type of the flow funciton

--- app/src/main/java/com/example/billionairehari/core/data/interfaces/TenantRepositoryInterface.kt
+++ app/src/main/java/com/example/billionairehari/core/data/interfaces/TenantRepositoryInterface.kt
@@ -10,8 +10,9 @@ interface TenantRepositoryInterface {
     fun getTenantsCardFlow(ownerId:String): Flow<List<TenantDao.TenantCardDetails>>
     suspend fun getTenantsCard(ownerId:String): List<TenantDao.TenantCardDetails>
     fun getTenant(tenantId:String,ownerId:String): Flow<TenantDao.TenantDetails>
-    fun getPaidCount(ownerId:String): Flow<TenantDao.RentPaid>
-    fun getNotPaidCount(ownerId:String) : Flow<TenantDao.RentNotPaid>
+    fun getPaidCount(ownerId:String): Flow<Int>
+    fun getNotPaidCount(ownerId:String) : Flow<Int>
+    fun getPartialPaidCount(ownerId:String): Flow<Int>
     fun getTenantRecentPayments(ownerId:String,tenantId:String): Flow<List<TenantDao.PaymentCard>>
     fun getTenantPayments(ownerId:String,tenantId:String): Flow<List<TenantDao.PaymentCard>>
     fun getTenantSearchCards(ownerId:String): Flow<List<TenantDao.TenantWithRoomRentCard>>

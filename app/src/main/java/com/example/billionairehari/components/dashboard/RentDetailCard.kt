31d25ca7d7bd0c62280cb2126c8ac4e0ccb0c7e3
HashiramaSenjuhari <truebillionhari@gmail.com>
2026-01-19 22:17:10 +0530

    Modified the rent info design

--- app/src/main/java/com/example/billionairehari/components/dashboard/RentDetailCard.kt
+++ app/src/main/java/com/example/billionairehari/components/dashboard/RentDetailCard.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -36,66 +40,73 @@ import com.example.billionairehari.ui.theme.robotoFontFamily
 import com.example.billionairehari.viewmodels.GetTenantPaidCountViewModel
 
 @Composable
-fun RentDetails(
+fun Details(
     rent_paid: GetTenantPaidCountViewModel = hiltViewModel()
 ){
     val paid = rent_paid.paid.collectAsState()
     val not_paid = rent_paid.not_paid.collectAsState()
-    ROw(
-        modifier = Modifier.fillMaxWidth()
-            .border(1.dp, color = Color.Black.copy(0.1f), shape = RoundedCornerShape(13.dp))
-            .padding(vertical = 13.dp, horizontal = 13.dp),
-        horizontalArrangement = Arrangement.spacedBy(13.dp)
+    val partial_paid = rent_paid.partial_paid.collectAsState()
+    Column(
+        modifier = Modifier.wrapContentHeight()
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(0.5f),
-            verticalArrangement = Arrangement.spacedBy(13.dp)
+        Text("Rent Info", fontSize = 16.sp,modifier = Modifier.offset(x = 16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(vertical = 13.dp, horizontal = 13.dp),
+            horizontalArrangement = Arrangement.spacedBy(13.dp),
+            verticalAlignment = Alignment.Top
         ) {
-            RentDetailCard(
-                modifier = Modifier.fillMaxWidth(),
-                type = RentDetailType.PAID,
-                count = paid.value.rentPaid
-            )
-            AppButton(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp))
-                    .fillMaxWidth(),
-                containerColor = Color.Black,
-                onClick = {
-                }
+            Column(
+                modifier = Modifier.fillMaxWidth(0.5f),
+                verticalArrangement = Arrangement.spacedBy(13.dp)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(6.dp)
-                ) {
-                    Text(
-                        "View All",
-                        color = Color.White
-                    )
-                }
+                RentDetailCard(
+                    modifier = Modifier.fillMaxWidth()
+                        .border(1.dp, color = Color.Black.copy(0.1f), shape = RoundedCornerShape(13.dp)),
+                    type = RentDetailType.PAID,
+                    count = paid.value,
+                )
+                RentDetailCard(
+                    modifier = Modifier.fillMaxWidth()
+                        .border(1.dp, color = Color.Black.copy(0.1f), shape = RoundedCornerShape(13.dp)),
+                    type = RentDetailType.PARTIAL_PAID,
+                    count = partial_paid.value,
+                )
             }
-        }
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(13.dp)
-        ) {
-            RentDetailCard(
-                modifier = Modifier.fillMaxWidth(),
-                type = RentDetailType.NOT_PAID,
-                count = not_paid.value.notPaid
-            )
-            AppButton(
+            Column(
                 modifier = Modifier.fillMaxWidth()
-                    .background(Color.White),
-                border = BorderStroke(2.dp, color = Color.Black.copy(0.1f)),
-                onClick = {},
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(13.dp))
+                    .shadow(elevation = 1.dp, shape = RoundedCornerShape(13.dp), spotColor = Color.Black.copy(0.1f))
+                    .background(Color.White)
+                    .border(1.dp, color = Color.Black.copy(0.1f), shape = RoundedCornerShape(13.dp)),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(6.dp)
-                ) {
-                    Icon(RemaindIcon, contentDescription = "", tint =  Color(0xFF1B1B1B), modifier =  Modifier.size(16.dp))
-                    Text("Remaind To Pay", fontSize = 12.sp, color =  Color(0xFF1B1B1B))
+                RentDetailCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    type = RentDetailType.NOT_PAID,
+                    count = not_paid.value
+                )
+                Box(
+                    modifier = Modifier.padding(horizontal = 6.dp, vertical = 6.dp)
+                ){
+                    AppButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp)
+                            .background(Color.White),
+                        border = BorderStroke(2.dp, color = Color.Black.copy(0.1f)),
+                        padding = PaddingValues(vertical = 13.dp),
+                        onClick = {}
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(6.dp)
+                        ) {
+                            Icon(RemaindIcon, contentDescription = "", tint =  Color(0xFF1B1B1B), modifier =  Modifier.size(16.dp))
+                            Text("Remaind To Pay", fontSize = 12.sp, color =  Color(0xFF1B1B1B))
+                        }
+                    }
                 }
             }
         }
@@ -104,7 +115,8 @@ fun RentDetails(
 
 enum class RentDetailType {
     PAID,
-    NOT_PAID
+    NOT_PAID,
+    PARTIAL_PAID
 }
 
 @Composable
@@ -113,30 +125,33 @@ fun RentDetailCard(
     type: RentDetailType,
     count:Int
 ){
-    Row(
-        modifier = Modifier.then(modifier)
-            .shadow(elevation = 1.dp, shape = RoundedCornerShape(13.dp), spotColor = Color.Black.copy(0.1f))
-            .background(Color.White)
-            .border(1.dp, color = Color.Black.copy(0.1f), shape = RoundedCornerShape(13.dp))
-            .clip(RoundedCornerShape(13.dp))
-            .padding(13.dp),
-        verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            if(type == RentDetailType.PAID){
-                "Paid"
-            }else {
-                "Not-Paid"
-            },
-            fontSize = 13.sp,
-            fontFamily = robotoFontFamily,
-            fontWeight = FontWeight.Medium
-        )
-        Column(
+    Column {
+        Row(
+            modifier = Modifier.then(modifier)
+                .shadow(elevation = 1.dp, shape = RoundedCornerShape(13.dp), spotColor = Color.Black.copy(0.1f))
+                .background(Color.White)
+                .clip(RoundedCornerShape(13.dp))
+                .padding(13.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(count.toString(), fontSize = 24.sp, fontWeight = FontWeight.Bold, fontFamily = robotoFontFamily,color =  if(type == RentDetailType.PAID) {Color.Black} else {Color.Red})
-            Text("Tenants",color = Color(0xFF909090), fontSize = 12.sp, fontFamily = robotoFontFamily)
+            Text(
+                if(type == RentDetailType.PAID){
+                    "Paid"
+                }else if(type == RentDetailType.PARTIAL_PAID){
+                    "Partial Paid"
+                }else {
+                    "Not-Paid"
+                },
+                fontSize = 13.sp,
+                fontFamily = robotoFontFamily,
+                fontWeight = FontWeight.Medium
+            )
+            Column(
+            ) {
+                Text(count.toString(), fontSize = 24.sp, fontWeight = FontWeight.Bold, fontFamily = robotoFontFamily,color =  if(type == RentDetailType.PAID) {Color.Black} else {Color.Red})
+                Text("Tenants",color = Color(0xFF909090), fontSize = 12.sp, fontFamily = robotoFontFamily)
+            }
         }
     }
 }

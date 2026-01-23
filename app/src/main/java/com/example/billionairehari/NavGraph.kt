e99d90cd1246a437f893cedaa70570c455a24880
HashiramaSenjuhari <truebillionhari@gmail.com>
2026-01-23 15:33:15 +0530

    refactor: added paymentSearchRoute And navigateToPaidInfos function

--- app/src/main/java/com/example/billionairehari/NavGraph.kt
+++ app/src/main/java/com/example/billionairehari/NavGraph.kt
@@ -17,6 +17,7 @@ object Screens {
     const val SETTING_SCREEN = "setting"
     const val ROOM_SEARCH_SCREEN = "room_search"
     const val TENANT_SEARCH_SCREEN = "tenant_search"
+    const val PAYMENT_SEARCH_SCREEN = "payment_search"
     const val AUTH_SCREEN = "auth"
     const val PAID_SCREEN = "paids"
     const val NOT_PAID_SCREEN = "not_paids"
@@ -28,6 +29,11 @@ object Arguments {
     const val TENANT_ID_ARGS = "tenant_id"
     const val SEARCH_ID_ARGS = "search_id"
     const val PAYMENT_ID_ARGS = "payment_id"
+    const val TENANT_FILTER_TYPE = "type"
+}
+
+object Query {
+    const val FILTER_QUERY = "filter"
 }
 
 object Destinations {
@@ -37,7 +43,7 @@ object Destinations {
     const val PAYMENTS_ROUTE = "${Screens.PAYMENTS_SCREEN}"
     const val PAYMENT_ROUTE = "${Screens.PAYMENTS_SCREEN}/{${Arguments.PAYMENT_ID_ARGS}}"
 
-    const val TENANTS_ROUTE  = Screens.TENANTS_SCREEN
+    const val TENANTS_ROUTE  = "${Screens.TENANTS_SCREEN}?${Query.FILTER_QUERY}={${Arguments.TENANT_FILTER_TYPE}}"
     const val TENANT_ROUTE = "${Screens.TENANTS_SCREEN}/{${Arguments.TENANT_ID_ARGS}}"
     const val TENANT_RENT_HISTORY = "${Screens.TENANT_RENT_SCREEN}/{${Arguments.TENANT_ID_ARGS}}"
     const val PROFILE_ROUTE = "${Screens.PROFILE_SCREEN}"
@@ -48,13 +54,15 @@ object Destinations {
 
     const val ROOM_SEARCH_ROUTE = "${Screens.ROOM_SEARCH_SCREEN}"
     const val TENANT_SEARCH_ROUTE = "${Screens.TENANT_SEARCH_SCREEN}"
+    const val PAYMENT_SEARCH_ROUTE = "${Screens.PAYMENT_SEARCH_SCREEN}"
     const val AUTH_ROUTE = "${Screens.AUTH_SCREEN}"
     const val TENANT_PAYMENT_HISTORY_ROUTE = "${Screens.TENANT_PAYMENTS_SCREEN}/{${Arguments.TENANT_ID_ARGS}}"
 }
 
 class NavigationAction (private val navController: NavHostController) {
     fun navigateToDashboard(){
-        navController.navigate(Destinations.DASHBOARD_ROUTE) {
+        navController.navigate(Destinations.DASHBOARD_ROUTE)
+        {
             popUpTo(navController.graph.startDestinationId) {
                 saveState = true
             }
@@ -118,31 +126,31 @@ class NavigationAction (private val navController: NavHostController) {
             restoreState = true
         }
     }
-    fun navigateToPaidPage(){
-        navController.navigate(Destinations.PAID_ROUTE){
-            popUpTo(navController.graph.startDestinationId){
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
-        }
-    }
-    fun navigateToNotPaidPage(){
-        navController.navigate(Destinations.NOT_PAID_ROUTE){
-            popUpTo(navController.graph.startDestinationId){
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
-        }
-    }
-    fun navigateToPartialPaidPage(){
-        navController.navigate(Destinations.PARTIAL_PAID_ROUTE){
-            popUpTo(navController.graph.startDestinationId){
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
-        }
-    }
+//    fun navigateToPaidPage(){
+//        navController.navigate(Destinations.PAID_ROUTE){
+//            popUpTo(navController.graph.startDestinationId){
+//                saveState = true
+//            }
+//            launchSingleTop = true
+//            restoreState = true
+//        }
+//    }
+//    fun navigateToNotPaidPage(){
+//        navController.navigate(Destinations.NOT_PAID_ROUTE){
+//            popUpTo(navController.graph.startDestinationId){
+//                saveState = true
+//            }
+//            launchSingleTop = true
+//            restoreState = true
+//        }
+//    }
+//    fun navigateToPartialPaidPage(){
+//        navController.navigate(Destinations.PARTIAL_PAID_ROUTE){
+//            popUpTo(navController.graph.startDestinationId){
+//                saveState = true
+//            }
+//            launchSingleTop = true
+//            restoreState = true
+//        }
+//    }
 }
